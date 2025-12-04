d86cb8326878208c95675f39d9e479b69a71fb40
Edoardo Fiore <edoardo.fiore@opiquad.it>
2025-12-04 10:38:17 +0100

    api path fix

--- frontend/config.php
+++ frontend/config.php
@@ -2,7 +2,7 @@
 // config.php
 
 // URL del backend API. Assicurati che sia raggiungibile dal server web.
-define('API_BASE_URL', 'http://127.0.0.1:8000');
+define('API_BASE_URL', 'http://127.0.0.1:8000/api');
 
 // Chiave API per autenticarsi con il backend.
 define('API_KEY', 'mysecretkey');
